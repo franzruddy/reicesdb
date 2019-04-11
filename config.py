7039013f2a661f65e0c43207ad67c9bf18a6138b
franzruddy <salinasfranzruddy@gmail.com>
2019-04-11 13:04:19 -0400

    bd remota

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
 import web
 
-db_host = 'localhost'
-db_name = 'raicesdb'
-db_user = 'raices'
-db_pw = 'raices.2019'
+db_host = 'lmag6s0zwmcswp5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'usejmam2npbb581f'
+db_user = 'y7llczkeg2yfbdup'
+db_pw = 'qalq50cosono2ds6'
+
+
+# import web
+
+# db_host = 'localhost'
+# db_name = 'raicesdb'
+# db_user = 'raices'
+# db_pw = 'raices.2019'
 
 db = web.database(
     dbn='mysql',
@@ -12,3 +20,6 @@ db = web.database(
     user=db_user,
     pw=db_pw
     )
+
+
+    
